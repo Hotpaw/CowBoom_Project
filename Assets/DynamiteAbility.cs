3fb24e07f839778acca928379204dae9c414d0d9
Hotpaw <94328693+Hotpaw@users.noreply.github.com>
2023-09-19 10:01:12 +0200

    weapon rotation

---
+++ Assets/DynamiteAbility.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DynamiteAbility : MonoBehaviour
+{
+
+    public GameObject Dynamite;
+  
+
+    public float fireRate = 3;
+
+    float timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 position = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+        transform.up = position;
+
+
+        if (Input.GetMouseButton(0) && timer > fireRate)
+        {
+
+
+            GameObject dynamiteClone = Instantiate(Dynamite, transform.position, Dynamite.transform.localRotation);
+            Vector2 DynamiteAim = position;
+          
+        }
+
+
+        timer += Time.deltaTime;
+    }
+}
+
+
